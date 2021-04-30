ffcb19e65cd931110d9667d4da942cb8f203a51c
Kenichi Hakiri <k.hakiri@gmail.com>
2021-04-30 17:24:05 +0900

    the prototype getting worked.

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import flask
+from ics import Calendar,Event
+import requests
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def main():
+    def make_allday_event_readable(timeline):
+        for e in timeline:
+            e.begin = e.begin.replace(hour=10, tzinfo='Asia/Tokyo')
+            e.end = e.end.replace(hour=19, tzinfo='Asia/Tokyo')
+            yield e
+
+    url = flask.request.args.get('url', 'https://calendar.google.com/calendar/ical/ja.japanese%23holiday%40group.v.calendar.google.com/public/basic.ics')
+    ical = Calendar(requests.get(url).text)
+    ical.timeline = list(make_allday_event_readable(ical.timeline))
+
+    return str(ical)
+
+if __name__ == '__main__':
+    app.run()
